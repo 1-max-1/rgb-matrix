699684519b5498b3c1032bc6b05ae83de2d94177
Maxh <44454544+1-max-1@users.noreply.github.com>
2024-12-04 15:55:44 +1300

    Added touch control to cycle through patterns

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <RGBMatrix.h>
 #include "PatternManager.h"
+#include <functional>
 
 // Number of ticks of duration of the the timer that updates the matrix
 // See https://deepbluembedded.com/esp32-timers-timer-interrupt-tutorial-arduino-ide/
@@ -16,6 +17,15 @@ const uint64_t MATRIX_TIMER_TICKS = 45; // Microseconds
 
 RGBMatrix matrix;
 PatternManager patterns(&matrix);
+
+typedef void (PatternManager::*PatternFunc)();
+PatternFunc patternList[6] = { &PatternManager::checker, &PatternManager::concentricSquares, &PatternManager::diagonalRainbowFade,
+						&PatternManager::fullHueFade, &PatternManager::spectrumAnalyzer, &PatternManager::tiles };
+uint8_t currentPatternIdx = 0;
+
+bool touchPinActive = false;
+bool lastTouchPinState = false;
+const uint8_t TOUCH_THRESHOLD = 80;
 
 void IRAM_ATTR doMatrixTick() {
 	matrix.tick();
@@ -34,5 +44,12 @@ void setup() {
 }
 
 void loop() {
-	patterns.checker();
+	PatternFunc currentPatternFunc = patternList[currentPatternIdx];
+	(patterns.*currentPatternFunc)(); // Call update function for current pattern
+
+	// Move to next pattern every time pin is touched
+	lastTouchPinState = touchPinActive;
+	touchPinActive = touchRead(13) < TOUCH_THRESHOLD;
+	if (touchPinActive && !lastTouchPinState)
+		currentPatternIdx = (currentPatternIdx + 1) % 6;
 }
